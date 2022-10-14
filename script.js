38f116d5f63724a2923421b61a8d63d1c6a1ef04
unknown <C:\Users\user\AppData\Roaming\The Bat!\Mail>
2022-10-14 15:12:24 +1000

    Initial commit with working form

---
+++ script.js
@@ -0,0 +1,89 @@
+let liningLength = document.getElementById('lining-length-input');
+let woodworkType = document.getElementById('woodwork-type');
+let liningWidth = woodworkType.options[woodworkType.selectedIndex].value;
+let wallOpeningCheckbox = document.getElementById('wall-opening-checkbox');
+let wallOpeningDiv = document.querySelector('.wall-openings');
+wallOpeningDiv.style.display = "none";
+let openingsCount = document.getElementById('opening-count');
+let countButton = document.getElementById('count-button');
+let wallHeight = document.getElementById('wall-height-input');
+let wallWidth = document.getElementById('wall-width-input');
+let resultDisplay = document.getElementById('result');
+let woodworkPrice = woodworkType.options[woodworkType.selectedIndex].getAttribute('price');
+
+woodworkType.addEventListener('change', () => {
+    liningWidth = woodworkType.options[woodworkType.selectedIndex].value;
+    woodworkPrice = woodworkType.options[woodworkType.selectedIndex].getAttribute('price');
+});
+
+let openingHeight = document.createElement('p');
+openingHeight.className = "opening-height";        
+openingHeight.append('Высота проема 1 (в мм): ');
+openingHeight.innerHTML += '<input class="opening-height-input" type="text">';
+
+let openingWidth = document.createElement('p');
+openingWidth.className = "opening-width";    
+openingWidth.append('Ширина проема 1 (в мм): ');
+openingWidth.innerHTML += '<input class="opening-width-input" type="text">';
+
+wallOpeningDiv.appendChild(openingHeight);
+wallOpeningDiv.appendChild(openingWidth);
+
+wallOpeningCheckbox.addEventListener('change', () => {
+    if (wallOpeningCheckbox.checked == true) {
+        wallOpeningDiv.style.display = "initial";
+    } else {
+        wallOpeningDiv.style.display = "none";
+    }
+});
+
+openingsCount.addEventListener('change', buildOpenings);
+
+function buildOpenings() {
+    let numberOfOpenings = openingsCount.value;
+    if (document.querySelectorAll('.opening-height')) {
+        document.querySelectorAll('.opening-height').forEach( input => {
+            input.remove();
+        });
+
+        document.querySelectorAll('.opening-width').forEach( input => {
+            input.remove();
+        });
+    }
+
+    for (let i = 0; i < numberOfOpenings; i++) {
+        let openingHeight = document.createElement('p');
+        openingHeight.className = "opening-height";        
+        openingHeight.append(`Высота проема ${i + 1} (в мм): `);
+        openingHeight.innerHTML += '<input class="opening-height-input" type="text">';
+
+        let openingWidth = document.createElement('p');
+        openingWidth.className = "opening-width";    
+        openingWidth.append(`Ширина проема ${i + 1} (в мм): `);
+        openingWidth.innerHTML += '<input class="opening-width-input" type="text">';
+
+        wallOpeningDiv.appendChild(openingHeight);
+        wallOpeningDiv.appendChild(openingWidth);
+    }
+}
+
+countButton.addEventListener('click', countLining);
+
+function countLining() {
+    let openingsOverall = 0;
+    
+    if (wallOpeningCheckbox.checked == true) {
+        let openingHeightDimensions = document.querySelectorAll('.opening-height-input');
+        let openingWidthDimensions = document.querySelectorAll('.opening-width-input');        
+
+        for (let i = 0; i < openingHeightDimensions.length; i++) {
+            openingsOverall += ((parseFloat(openingHeightDimensions[i].value) * parseFloat(openingWidthDimensions[i].value)))
+        }
+    }    
+
+    let result = Math.ceil((((parseFloat(wallHeight.value) * parseFloat(wallWidth.value) - openingsOverall) / parseFloat(liningLength.value) / liningWidth)) / 1000);    
+    
+    if (result > 0) {
+        resultDisplay.innerHTML = `Необходимое количество вагонки: ${result} шт.` + '<br>' + `Стоимость: ${result * woodworkPrice * (liningLength.value/1000)} руб.`;
+    }
+}
