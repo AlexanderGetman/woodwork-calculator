cdbcc2a1c5aa15c3dc48168eae00c36f5613310a
unknown <C:\Users\user\AppData\Roaming\The Bat!\Mail>
2022-10-14 16:07:17 +1000

    Added round openings to calculation

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ let wallHeight = document.getElementById('wall-height-input');
 let wallWidth = document.getElementById('wall-width-input');
 let resultDisplay = document.getElementById('result');
 let woodworkPrice = woodworkType.options[woodworkType.selectedIndex].getAttribute('price');
+let wallHoleCheckbox = document.getElementById('wall-holes-checkbox');
+let wallHoleDiv = document.querySelector('.wall-holes');
+wallHoleDiv.style.display = "none";
+let holesCount = document.getElementById('holes-count');
+
 
 woodworkType.addEventListener('change', () => {
     liningWidth = woodworkType.options[woodworkType.selectedIndex].value;
@@ -67,10 +72,46 @@ function buildOpenings() {
     }
 }
 
+let holeDiameter = document.createElement('p');
+holeDiameter.className = "diameter";        
+holeDiameter.append('Диаметр круглого отверстия 1 (в мм): ');
+holeDiameter.innerHTML += '<input class="diameter-input" type="text">';
+
+wallHoleDiv.appendChild(holeDiameter);
+
+wallHoleCheckbox.addEventListener('change', () => {
+    if (wallHoleCheckbox.checked == true) {
+        wallHoleDiv.style.display = "initial";
+    } else {
+        wallHoleDiv.style.display = "none";
+    }
+});
+
+holesCount.addEventListener('change', buildHoles);
+
+function buildHoles() {
+    let numberOfHoles = holesCount.value;
+    if (document.querySelectorAll('.diameter')) {
+        document.querySelectorAll('.diameter').forEach( input => {
+            input.remove();
+        });
+    }
+
+    for (let i = 0; i < numberOfHoles; i++) {
+        let holeDiameter = document.createElement('p');
+        holeDiameter.className = "diameter";        
+        holeDiameter.append(`Диаметр круглого отверстия ${i + 1} (в мм): `);
+        holeDiameter.innerHTML += '<input class="diameter-input" type="text">';
+        
+        wallHoleDiv.appendChild(holeDiameter);
+    }
+}
+
 countButton.addEventListener('click', countLining);
 
 function countLining() {
     let openingsOverall = 0;
+    let holesOverall = 0;
     
     if (wallOpeningCheckbox.checked == true) {
         let openingHeightDimensions = document.querySelectorAll('.opening-height-input');
@@ -79,9 +120,17 @@ function countLining() {
         for (let i = 0; i < openingHeightDimensions.length; i++) {
             openingsOverall += ((parseFloat(openingHeightDimensions[i].value) * parseFloat(openingWidthDimensions[i].value)))
         }
-    }    
+    }
 
-    let result = Math.ceil((((parseFloat(wallHeight.value) * parseFloat(wallWidth.value) - openingsOverall) / parseFloat(liningLength.value) / liningWidth)) / 1000);    
+    if (wallHoleCheckbox.checked == true) {
+        let holeDimensions = document.querySelectorAll('.diameter-input');
+
+        for (let i = 0; i < holeDimensions.length; i++) {
+            holesOverall += (Math.PI * (parseFloat(holeDimensions[i].value) ** 2) / 4);
+        }
+    }
+
+    let result = Math.ceil(((((parseFloat(wallHeight.value) * parseFloat(wallWidth.value)) - openingsOverall - holesOverall) / parseFloat(liningLength.value) / liningWidth)) / 1000);    
     
     if (result > 0) {
         resultDisplay.innerHTML = `Необходимое количество вагонки: ${result} шт.` + '<br>' + `Стоимость: ${result * woodworkPrice * (liningLength.value/1000)} руб.`;
